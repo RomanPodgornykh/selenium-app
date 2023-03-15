200633d7b134a241df4729c4b2e674f7b6e037a6
Roman Podhornykh <romanpodgornykh@gmail.com>
2023-03-15 22:44:22 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int monthNumber = 2;
+
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Jan");
+            case 2:
+                System.out.println("Feb");
+            case 3:
+                System.out.println("Mar");
+
+            default:
+                System.out.println("Can't find month");
+        }
+
+    }
+
+}
